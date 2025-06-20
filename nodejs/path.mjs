56e6c8b13fab2044ab9a5364b91e10acdaa7cfec
ismanong <526443791@qq.com>
2025-06-20 14:43:46 +0000

    change path

--- nodejs/path.mjs
+++ nodejs/path.mjs
@@ -2,4 +2,20 @@ import path, { dirname } from 'node:path'
 import { fileURLToPath, pathToFileURL } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
-export const __dirname = dirname(__filename)
+const __dirname = dirname(__filename)
+
+// 获取调用者的路径
+export function getCallerPath() {
+  const origPrepareStackTrace = Error.prepareStackTrace;
+  Error.prepareStackTrace = (_, stack) => stack;
+
+  const err = new Error();
+  const stack = err.stack;
+
+  Error.prepareStackTrace = origPrepareStackTrace;
+
+  // stack[0] 是当前函数，stack[1] 是调用者
+  const callerFile = stack[1].getFileName();
+
+  return path.dirname(callerFile);
+}
